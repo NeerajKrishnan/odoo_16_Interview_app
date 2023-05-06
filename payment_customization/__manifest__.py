4a361a1b2bd7b6c5bafb3ec0305e3b7de5a9c255
Neeraj <neeraj@pop-os.localdomain>
2023-05-06 14:45:30 +0530

    First Commit

---
+++ payment_customization/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name' : 'Payment Customization',
+    'version' : '0.0',
+    'summary': 'Payment & Customization',
+    'sequence': 1,
+    'description': """
+    """,
+    'category': 'Accounting/Accounting',
+    'website': 'https://www.linkedin.com/in/neeraj-krishnan-6293b4121/',
+    'depends' : ['account'],
+    'data': [
+
+    ],
+
+
+    'license': 'LGPL-3',
+}
